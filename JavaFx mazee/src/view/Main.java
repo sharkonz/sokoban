1f8bff6d6adeaa63381ec936975b8586cd075dae
Charcon <sharkonz@gmail.com>
2017-01-18 14:20:03 +0200

    xzcz

--- JavaFx mazee/src/view/Main.java
+++ JavaFx mazee/src/view/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 //checking
+//sadsaldkad
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
